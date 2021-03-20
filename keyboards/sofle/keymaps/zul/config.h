39221b10af939ecb15231df05d9599a27904f9af
Chris Halliwell <chris.halliwell@saatchi.co.uk>
2021-03-20 20:40:56 +0000

    oled timeout fixes

--- keyboards/sofle/keymaps/zul/config.h
+++ keyboards/sofle/keymaps/zul/config.h
@@ -1,7 +1,6 @@
 #pragma once
-#define OLED_TIMEOUT 120000
+#define OLED_TIMEOUT 240000
 /* The way how "handedness" is decided (which half is which),
 see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options.
 */
-#define OLED_SCROLL_TIMEOUT 1000
